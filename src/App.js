328a2ef66c3e8afa9ab30508a883821173fc9b10
Maged Bebawy <mrmaged1991@gmail.com>
2020-12-10 08:54:28 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    //jsx elements
+    <div>
+      <h1>Header</h1>
+      <p>Some Paragraph</p>
+    </div>
+  );
+}
+
+function App1() {
+  return (
+    //jsx elements
+    <div>
+      <h1>Header1</h1>
+      <p>Some Paragraph1</p>
+    </div>
+  );
+}
+
+export {App, App1};
